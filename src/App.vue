2ca35c6682571a5d5cebff785e0f1bf71c8aae8b
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-08-03 15:32:36 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/singup">Singup</router-link>
   </nav>
   <router-view/>
 </template>
